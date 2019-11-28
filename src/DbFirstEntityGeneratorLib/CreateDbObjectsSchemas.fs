3c11cf3f44290775b4459bfc7fca21e9700d1b1a
Christoph Pletz <christoph.pletz@trivadis.com>
2019-11-28 19:49:35 +0100

    dbobjects schema generation

--- src/DbFirstEntityGeneratorLib/CreateDbObjectsSchemas.fs
+++ src/DbFirstEntityGeneratorLib/CreateDbObjectsSchemas.fs
@@ -4,7 +4,6 @@ open System
 open System.IO
 open System.Linq
 open CommandLine
-open Serilog
 open GeneratorConfiguration
 open SqlMetadata
 open Microsoft.EntityFrameworkCore
@@ -16,9 +15,7 @@ module CreateDbObjectsSchemas =
         use db = createInformationContext connStr
         let columnInfos = db.Columns.AsNoTracking().ToArray()
 
-        let tablesAndColumns = 
-            columnInfos
-            |> Array.groupBy (fun x -> sprintf "%s.%s" x.tableSchema x.tableName)
+        let tablesAndColumns = columnInfos |> Array.groupBy (fun x -> sprintf "%s.%s" x.tableSchema x.tableName)
         tablesAndColumns
 
     [<Verb("schemas", HelpText = "Create json schemas for db contexts.")>]
@@ -62,52 +59,40 @@ module CreateDbObjectsSchemas =
             writeJsonType JObject
             writePropertyWithObject "properties" propertyWriter
 
+        let writePropertyWithSchemaProperties propName propertyWriter =
+            writePropertyWithObject propName (fun () -> writeSchemaObjectWithProperties propertyWriter)
+
         let writeSchemaProperty = writeSchemaPropertyWithPattern None
 
         let writeDefinitions() =
             writePropertyWithObject "definitions"
                 (fun () ->
-                writePropertyWithObject "columnOverrides" (fun () ->
-                    writeSchemaObjectWithProperties
-                        (fun () -> 
-                            writeSchemaProperty "isNullable" JBoolean
-                            writeSchemaProperty "dataType" JString
-                            writeSchemaPropertyWithPattern identifierPattern "propertyName" JString))
-                    )
+                writePropertyWithSchemaProperties "columnOverrides" (fun () ->
+                    writeSchemaProperty "isNullable" JBoolean
+                    writeSchemaProperty "dataType" JString
+                    writeSchemaPropertyWithPattern identifierPattern "propertyName" JString))
 
-        let writeTables () =
-            writePropertyWithObject "tables"
-                (fun () -> 
-                    writeSchemaObjectWithProperties
-                        (fun () -> 
-                            tablesAndColumns
-                            |> Array.iter ( fun (table, columns) -> 
-                                                writePropertyWithObject table
-                                                    (fun () -> 
-                                                        writeSchemaObjectWithProperties
-                                                            (fun () -> 
-                                                                writeSchemaPropertyWithPattern identifierPattern "typeName" JString
-                                                                writePropertyWithObject "columns"
-                                                                    (fun () ->
-                                                                        writeSchemaObjectWithProperties
-                                                                            (fun () ->   
-                                                                                columns
-                                                                                |> Array.iter (fun col ->
-                                                                                                 writePropertyWithObject col.columnName
-                                                                                                    (fun () ->
-                                                                                                        writer.WriteString ("$ref", "#/definitions/columnOverrides")
-                                                                                                 ))
-                                                                                                 )
-                                                                                                 
-                                                            )
-                                                    ))
-                        )
-                    ))        
+        let writeTables() =
+            writePropertyWithSchemaProperties "tables" (fun () ->
+                tablesAndColumns
+                |> Array.iter (fun (table, columns) ->
+                    writePropertyWithObject table (fun () ->
+                        writeSchemaObjectWithProperties (fun () ->
+                            writeSchemaPropertyWithPattern identifierPattern "typeName" JString
+                            writePropertyWithSchemaProperties "columns"
+                                (fun () ->
+                                columns
+                                |> Array.iter
+                                    (fun col ->
+                                    writePropertyWithObject col.columnName
+                                        (fun () -> writer.WriteString("$ref", "#/definitions/columnOverrides"))))
+
+                            ))))
 
         writer.WriteStartObject()
         writer.WriteString("$schema", "http://json-schema.org/draft-04/schema#")
         writeDefinitions()
-        writeSchemaObjectWithProperties writeTables 
+        writeSchemaObjectWithProperties writeTables
         writer.WriteEndObject()
 
     let createDbObjectSchema (name: string) (config: DbContextConfig) =
@@ -123,7 +108,6 @@ module CreateDbObjectsSchemas =
         use sr = new StreamReader(jsonStream)
         let json = sr.ReadToEnd()
         File.WriteAllText(outputPath, json)
-
         0
 
     let createDbObjectsSchemas (opts: CreateDbObjectsSchemaOptions) =
@@ -131,7 +115,6 @@ module CreateDbObjectsSchemas =
         let specifiedDir =
             if not (String.IsNullOrWhiteSpace opts.directory) then Some(opts.directory)
             else None
-
         match createFromDirectory specifiedDir with
         | Ok genConfig ->
             let results =
@@ -141,27 +124,3 @@ module CreateDbObjectsSchemas =
             results |> List.max
         | Error _ -> 1
 
-//let genConfigDir = DirectoryResolver.resolve None specifiedDir
-//if not (Directory.Exists genConfigDir) then
-//    Log.Error(sprintf "The directory '%s' does not exist." genConfigDir)
-//    1
-//else
-//    match createFromDirectory
-//    let genConfigPath = Path.Combine(genConfigDir, "genconfig.json")
-//    if not (File.Exists genConfigPath) then
-//        Log.Error(sprintf "The genconfig file at path '%s' could not be found." genConfigPath)
-//        1
-//    else
-//        let json = File.ReadAllText genConfigPath
-//        let genConfigResult = createFromJson json
-//        match genConfigResult with
-//        | Error msgs ->
-//            Log.Error(sprintf "Failed to parse genconfig file '%s'. Problems:" genConfigPath)
-//            msgs |> List.iter Log.Information
-//            1
-//        | Ok genConfig ->
-//            let results =
-//                genConfig.dbContexts
-//                |> Seq.map (fun x -> createDbObjectSchema x.Key x.Value)
-//                |> Seq.toList
-//            results |> List.max
